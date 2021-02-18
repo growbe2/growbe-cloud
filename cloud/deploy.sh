cba131bee7d1ffc79c166e3131d4d7d9eb0537c4
William Quintal <wquintal@alborea.com>
2021-02-17 19:10:39 -0500

    pipeline

--- cloud/deploy.sh
+++ cloud/deploy.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+docker-compose -f ./cloud/cloud-dev.docker-compose.yml -f ./cloud/prod.docker-compose.override.yml stop
 docker-compose -f ./cloud/cloud-dev.docker-compose.yml -f ./cloud/prod.docker-compose.override.yml up -d
