c865e8ab1e105187bfa6976845dea0c3d490685b
William Quintal <wquintal@alborea.com>
2021-03-21 18:12:03 -0400

    version(1.0.2)

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+docker-compose -f ./cloud/cloud-dev.docker-compose.yml pull growbe-cloud growbe-cloud-watcher sso frontend_dev
 docker-compose -f ./cloud/cloud-dev.docker-compose.yml stop
 docker-compose -f ./cloud/cloud-dev.docker-compose.yml up -d
