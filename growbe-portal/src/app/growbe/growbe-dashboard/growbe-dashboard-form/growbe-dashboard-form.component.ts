b3d108ee3575992f6a70b4f3e25bb5bceaff0e7b
William Quintal <wquintal@alborea.com>
2021-03-24 22:56:50 -0400

    feat(portal:routing) removing resolver to use mixin instead

--- growbe-portal/src/app/growbe/growbe-dashboard/growbe-dashboard-form/growbe-dashboard-form.component.ts
+++ growbe-portal/src/app/growbe/growbe-dashboard/growbe-dashboard-form/growbe-dashboard-form.component.ts
@@ -3,7 +3,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '@berlingoqc/auth';
 import { fuseAnimations } from '@berlingoqc/fuse';
 import { AutoFormData } from '@berlingoqc/ngx-autoform';
+import { GrowbeDashboard } from '@growbe2/ngx-cloud-api';
 import { GrowbeDashboardAPI } from '../../api/growbe-dashboard';
+import { GrowbeMainboardAPI } from '../../api/growbe-mainboard';
 
 @Component({
   selector: 'app-growbe-dashboard-form',
@@ -44,16 +46,18 @@ export class GrowbeDashboardFormComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private dashboardAPI: GrowbeDashboardAPI,
+    private bAPI: GrowbeMainboardAPI,
     private router: Router,
     private authService: AuthService,
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
   onSubmit(data) {
     console.log(this.authService);
-    this.dashboardAPI.post({name: data.dashboard.name, userId: this.authService.profile.id}).subscribe((item) => {
+    this.dashboardAPI.post({name: data.dashboard.name, userId: this.authService.profile.id}).subscribe((item: GrowbeDashboard) => {
       console.log(item);
       this.router.navigate(['/','dashboard',item.id])
     });
--- growbe-portal/src/app/growbe/growbe-manager/growbe-manager.routing.ts
+++ growbe-portal/src/app/growbe/growbe-manager/growbe-manager.routing.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { GrowbeManagerDashboardComponent } from "./growbe-manager-dashboard/growbe-manager-dashboard.component";
 import { GrowbeManagerDetailComponent } from "./growbe-manager-detail/growbe-manager-detail.component";
-import { GrowbeMainboardResolver } from '../resolvers/mainboard';
 import { GrowbeMainboardAPI } from "../api/growbe-mainboard";
 
 const routes: Routes = [
