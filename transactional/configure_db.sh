e477bf0277072e5349d94db338748d3aa89450b5
William Quintal <william.quintal@powerside.com>
2021-06-10 16:27:02 -0400

    add integration env and test

--- transactional/configure_db.sh
+++ transactional/configure_db.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
 echo "Running script to setup database"
+
+
+psql $DB_URL -f ./transactional/growbemoduledef.sql
+psql $DB_URL -f ./transactional/growbewarningkey.sql
+psql $DB_URL -f ./transactional/user.sql
---
+++ transactional/growbewarningkey.sql
@@ -0,0 +1,3 @@
+DELETE FROM public.growbewarningkey;
+INSERT INTO public.growbewarningkey ("key",multi) VALUES
+	 ('RTC_OFFSET','false');
