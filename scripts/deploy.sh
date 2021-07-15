1a9c382bc110fda05071f0d9385a75d8d252df01
William Quintal <william.quintal@powerside.com>
2021-07-14 23:31:03 -0400

    fix(deploy): remove fluentd stack

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker-compose -f ./docker/cloud-dev.docker-compose.yml -f ./docker/fluentd.docker-compose.yml pull growbe-cloud growbe-cloud-watcher sso frontend_dev nms
-docker-compose -f ./docker/cloud-dev.docker-compose.yml -f ./docker/fluentd.docker-compose.yml stop
-docker-compose -f ./docker/cloud-dev.docker-compose.yml -f ./docker/fluentd.docker-compose.yml up -d
+docker-compose -f ./docker/cloud-dev.docker-compose.yml pull growbe-cloud growbe-cloud-watcher sso frontend_dev nms
+docker-compose -f ./docker/cloud-dev.docker-compose.yml stop
+docker-compose -f ./docker/cloud-dev.docker-compose.yml up -d
