47d478ede250b7f4e12f34c1f94f80a3de0d0ffb
William Quintal <william.quintal@powerside.com>
2021-08-07 14:57:02 -0400

    fix(cloud): module config use auth by module with old endpoint

--- growbe-cloud/src/cloud/controllers/growbe-module.controllers.ts
+++ growbe-cloud/src/cloud/controllers/growbe-module.controllers.ts
@@ -1,7 +1,7 @@
 import {param, post, requestBody} from '@loopback/openapi-v3';
 import {GrowbeModuleService} from '../../services';
 import {service} from '@loopback/core';
-import { authorizeGrowbe } from '../authorization';
+import { authorizeGrowbe, getMainboardByModule } from '../authorization';
 
 export class GrowbeModuleController {
   constructor(
@@ -9,12 +9,12 @@ export class GrowbeModuleController {
     private growbeModuleService: GrowbeModuleService,
   ) {}
 
-  @post('/growbes/{boardId}/modules/{id}/config')
+  @post('/growbeModules/{id}/config')
   @authorizeGrowbe({
     growbeIdIndex: 0,
+    getFunc: getMainboardByModule,
   })
   setConfig(
-    @param.path.string('boardId') boardId: string, 
     @param.path.string('id') id: string, @requestBody() config: any
   ) {
     return this.growbeModuleService.setModuleConfig(id, config);
