335e7cd53bc89eb603a2327b4f1787d5d7362e02
William Quintal <william95quintalwilliam@outlook.com>
2022-11-11 19:42:40 -0500

    fix(ci): use doctl to get cluster credentials and config

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 mkdir -p ./helm/charts
-#helm pull oci://ghcr.io/growbe2/growbe-cloud-chart --version="0.2.0" --destination ./helm/charts/
+helm pull oci://ghcr.io/growbe2/growbe-cloud-chart --version="0.2.0" --destination ./helm/charts/
 helm upgrade growbe-cloud ./helm --install --wait --atomic $@
